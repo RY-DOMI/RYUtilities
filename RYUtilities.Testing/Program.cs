c7cc33986faeca0044b051d10649fa01117cd577
RY-DOMI <dominik.auer04@gmail.com>
2020-03-26 01:45:42 +0100

    Initial commit

---
+++ RYUtilities.Testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using RYUtilities;
+
+namespace RYUtilities.Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logging.WriteInfo("Loading api...");
+            Logging.WriteDebug(">> Starting WEB-Service...");
+            Logging.WriteError("Something went wrong!");
+        }
+    }
+}
---
+++ RYUtilities/Logging.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RYUtilities
+{
+    public static class Logging
+    {
+        /// <summary>Writes an information message 
+        /// into the console.</summary>
+        public static void WriteInfo(string message, bool newLine = true)
+        {
+            Write("Info", message, ConsoleColor.Cyan, newLine);
+        }
+
+        /// <summary>Writes an information message 
+        /// into the console.</summary>
+        public static void WriteDebug(string message, bool newLine = true)
+        {
+            Write("Debug", message, ConsoleColor.Yellow, newLine);
+        }
+
+        /// <summary>Writes an error message 
+        /// into the console.</summary>
+        public static void WriteError(string message, bool newLine = true)
+        {
+            Write("Error", message, ConsoleColor.Red, newLine);
+        }
+
+        /// <summary>Writes an message 
+        /// into the console.</summary>
+        private static void Write(string prefix, string message, ConsoleColor color = ConsoleColor.White, bool newLine = true)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("[");
+            
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(DateTime.Now.ToString("HH:mm:ss"));
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("/");
+
+            Console.ForegroundColor = color;
+            Console.Write(prefix);
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("] ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(message + (newLine ? "\n" : ""));
+            Console.ResetColor();
+        }
+    }
+}
